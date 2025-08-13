36941615c80cd8faf34437d73ce28323a996aaa3
Manish Dait <daitmanish88@gmail.com>
2025-08-13 14:52:16 +0530

    added missing functionality for cardview

--- cloud-share-client/src/app/components/card-view/card-view.component.ts
+++ cloud-share-client/src/app/components/card-view/card-view.component.ts
@@ -1,7 +1,11 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { getSize, timestampToDate } from '../../shared/utils';
 import { File } from '../../types/file.type';
+import { FileService } from '../../service/file.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.state';
+import { deleteFile, updateFile } from '../../store/file/file.action';
 
 @Component({
   selector: 'app-card-view',
@@ -11,7 +15,11 @@ import { File } from '../../types/file.type';
 })
 export class CardViewComponent {
   file = input.required<File>();
+  
+  fileService = inject(FileService);
   menuToggle = signal(false);
+
+  constructor(private store: Store<AppState>) {}
 
   toggleMenu() {
     this.menuToggle.update(toggle => !toggle);
@@ -23,5 +31,47 @@ export class CardViewComponent {
 
   parseSize(size: number): string {
     return getSize(size);
+  }
+
+  toggleVisiblity(toggle: boolean) {
+    this.fileService.toggleVisiblity(this.file().uuid, toggle).subscribe({
+      next: (res) => {
+        this.store.dispatch(updateFile({file: res}));
+      },
+      error: (err) => {
+        
+      }
+    })
+  }
+
+  deleteFile() {
+    this.fileService.deleteFile(this.file().uuid).subscribe({
+      next: (res) => {
+        this.store.dispatch(deleteFile({uuid: res['deleted']}));
+        console.log(res);
+      },
+      error: (err) => {
+      }
+    })
+  }
+
+  download() {
+    this.fileService.downloadPublicFile(this.file().uuid).subscribe({
+      next: (res) => {
+        const fileName = res.headers.get('X-File-Name') as string || 'download';
+        const blob = new Blob([res.body!], { type: res.headers.get('Content-Type') as string });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      }
+    })
+  }
+
+  copyLink() {
+    navigator.clipboard.writeText(`http://localhost:4200/files/${this.file().uuid}`);
   }
 }
